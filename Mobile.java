583d316e07ed9bd3d3c8ceaa76aff32856b8f316
Arthi12345678 <139788891+Arthi12345678@users.noreply.github.com>
2023-07-18 22:12:07 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,24 @@
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileName="motorola";
+		System.out.println("mobileName       "+mobileName);
+		char mobileLogo='m';
+		System.out.println("mbileLogo        "+mobileLogo);
+		short noOfMobilePiece=1;
+		System.out.println("noOfMobilePiece  "+noOfMobilePiece);
+		int mobileModelNo=62;
+		System.out.println("mobileModelNo    "+mobileModelNo);
+		long mobileNumber=9658083172L;
+		System.out.println("mobileNumber     "+mobileNumber);
+	    float mobilePrice=15000;
+	    System.out.println("mobilePrice      "+mobilePrice);
+	    boolean isDamaged=false;
+	    System.out.println("isDamaged       "+isDamaged);
+		
+		
+		
+		
+	}
+
+}
---
+++ TwoWheeler.java
@@ -0,0 +1,24 @@
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels=4;
+		System.out.println("noOfWheels    "+noOfWheels);
+		short noOfMirrors=3;
+		System.out.println("noOfMirrors   "+noOfWheels);
+		long chasisNumber=345789888633L;
+		System.out.println("chasisNumber  "+chasisNumber);
+		boolean isPunctured=true;
+		System.out.println("isPunctured   "+isPunctured);
+		String bikeName="Royal Enfield";
+		System.out.println("bikeName      "+bikeName);
+		double runningKm=556.765;
+		System.out.println("runningKm     "+runningKm);
+		
+		
+		
+		
+	}
+
+	
+
+}
